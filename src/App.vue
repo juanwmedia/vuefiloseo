3126848fbaa8762851fcf710942f063edfc24b66
@juanwmedia <juan@wmedia.es>
2019-07-23 21:03:03 +0200

    Navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <Navegacion></Navegacion>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import Navegacion from './components/Navegacion';
+  export default  {
+    name: 'App',
+    components: {
+      Navegacion
+    }
+  }
+</script>
 
 <style lang="scss">
 
---
+++ src/components/Navegacion.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav class="navbar has-background-light" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div class="navbar-brand">
+                <router-link class="navbar-item" to="/">
+                    <img src="../assets/vuefilos.png">
+                </router-link>
+
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <router-link class="navbar-item" to="/">Portada</router-link>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link class="button is-primary" to="/registro"><strong>Registro</strong></router-link>
+                            <router-link class="button is-light" to="/login">Login</router-link>
+                            <router-link class="button is-light" to="/perfil">Perfil</router-link>
+                            <a class="button is-light" href="#">Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    import { bulmaHeader } from "../assets/utils";
+    export default {
+        name: "Navegacion",
+        mounted() {
+            bulmaHeader();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
