953585ebf20b15fbb4aa65bfa2be736dce7e3046
@juanwmedia <juan@wmedia.es>
2019-07-24 23:13:19 +0200

    Votar

--- src/components/Recurso.vue
+++ src/components/Recurso.vue
@@ -36,7 +36,15 @@
 
             </section>
             <footer class="modal-card-foot">
-                <button class="button is-danger">Votar recurso</button>
+                <span v-if="!usuario">
+                    <router-link to="/registro">Regístrate</router-link> para poder votar.
+                </span>
+
+                <span v-else-if="votado">
+                    Voto registrado.
+                </span>
+
+                <button v-else @click="votar" class="button is-danger" :class="{ 'is-loading': trabajando }">Votar recurso</button>
             </footer>
         </div>
     </div>
@@ -45,21 +53,55 @@
 <script>
     import { mapState } from 'vuex';
     import moment from 'moment';
+    const fb = require('../firebase.js');
     export default {
         name: "Recurso",
         created() {
             this.indice = this.recursos.findIndex(recurso => recurso.id === this.id);
             this.recurso = this.recursos[this.indice];
+
+            if (this.usuario) {
+                let votoId = this.usuario.uid + '_' + this.recurso.id;
+                fb.votosColeccion.doc(votoId).get().then(doc => {
+                   if (!doc.exists) {
+                       this.votado = false;
+                   }
+                });
+            }
         },
         data() {
             return {
                 indice: null,
                 recurso: {},
+                trabajando: false,
+                votado: true,
             }
         },
         props: ['id'],
         computed: {
             ...mapState(['usuario', 'recursos', 'perfil'])
+        },
+        methods: {
+            votar() {
+                this.trabajando = true;
+
+                let votoId = this.usuario.uid + '_' + this.recurso.id;
+
+                fb.votosColeccion.doc(votoId).set({
+                   recursoId: this.recurso.id,
+                   usuarioId: this.usuario.uid,
+                }).then(() =>{
+                    fb.recursosColeccion.doc(this.recurso.id).update({
+                       votos: this.recurso.votos + 1,
+                    }).then(()=> {
+                        this.votado = true;
+                    }).catch(error => {
+                        console.error(error);
+                    });
+                }).catch(error => {
+                    console.error(error);
+                }).finally(() => this.trabajando = false);
+            }
         },
         filters: {
             fechaHace(valor) {
