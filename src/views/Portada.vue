9cd12a695bc4d96d79e61585ca6a78f37a0f5f08
@juanwmedia <juan@wmedia.es>
2019-07-24 20:35:04 +0200

    Añadir recursos

--- src/views/Portada.vue
+++ src/views/Portada.vue
@@ -53,31 +53,42 @@
                 <!-- Agregar recurso -->
                 <div class="column is-offset-1">
                     <section class="section has-background-light">
-                        <form>
+                        <form @submit.prevent="agregar">
                             <div class="field">
                                 <label class="label">Título del recurso</label>
                                 <div class="control">
-                                    <input class="input" type="text" required />
+                                    <input v-model.trim="titulo" class="input" type="text" required />
                                 </div>
                             </div>
                             <div class="field">
                                 <label class="label">URL del recurso</label>
                                 <div class="control">
-                                    <input class="input" type="text" required />
+                                    <input v-model.trim="url" class="input" type="text" required />
                                 </div>
                             </div>
                             <div class="field">
                                 <label class="label">Descripción del recurso</label>
                                 <div class="control">
-                                    <textarea class="textarea" required></textarea>
+                                    <textarea v-model.trim="descripcion" class="textarea" required></textarea>
                                 </div>
                             </div>
-                            <div class="field is-grouped">
+
+
+                            <div v-if="usuario" class="field is-grouped">
                                 <div class="control">
-                                    <button type="submit" class="button is-link">Agregar recurso</button>
+                                    <button type="submit" class="button is-link" :class="{'is-loading': trabajando}">Agregar recurso</button>
                                 </div>
                             </div>
+
+                            <p v-else>
+                                Para añadir recursos debes <router-link to="/login">autentificarte</router-link> en Vuefilos.
+                            </p>
+
                         </form>
+
+                        <Error v-show="mensajeError" :mensaje="mensajeError"></Error>
+                        <Exito @cerrarMensaje="mensajeExito = ''" v-show="mensajeExito" :mensaje="mensajeExito"></Exito>
+
                     </section>
                 </div>
             </div>
@@ -87,9 +98,55 @@
 
 <script>
     // @ is an alias to /src
-
+    const fb = require('../firebase.js');
+    import Exito from '../components/Exito';
+    import Error from '../components/Error';
+    import { mapState } from 'vuex';
     export default {
         name: 'Portada',
+        data() {
+            return {
+                titulo: '',
+                url: '',
+                descripcion: '',
+                trabajando: false,
+                mensajeError: '',
+                mensajeExito: ''
+            }
+        },
+        methods: {
+            agregar() {
+                this.trabajando = true;
+                this.mensajeError = '';
+
+                fb.recursosColeccion.add({
+                    cuando: new Date(),
+                    titulo: this.titulo,
+                    url: this.url,
+                    descripcion: this.descripcion,
+                    userId: this.usuario.uid,
+                    nombre: this.perfil.nombre,
+                    comentarios: 0,
+                    votos: 0,
+                }).then(() => {
+                    this.mensajeExito = 'Recurso añadido con éxito';
+                    this.limpiar();
+                }).catch(error => {
+                    console.error(error);
+                    this.mensajeError = error.message;
+                }).finally(()=> this.trabajando = false);
+            },
+            limpiar() {
+                this.titulo = this.descripcion = this.url = '';
+            }
+        },
+        computed: {
+            ...mapState(['usuario', 'perfil']),
+        },
+        components: {
+            Exito,
+            Error,
+        }
     }
 </script>
 
